425ab813d4fd6409de01bcc98c6facd4d2cd66e1
Luiz Lopes <luizlopes12@outlook.com>
2022-03-02 19:10:10 -0300

    Container style

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Title, Paragraph } from './style'
+import { Container } from '../../styles/globalStyles'
 const Login = () => {
   return (
-    <>
+    <Container>
         <Title isRed={false}>Login <small>Opa</small></Title>
         <Paragraph>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</Paragraph>
-    </>
+    </Container>
   )
 }
 
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
 *{
@@ -9,6 +9,7 @@ export default createGlobalStyle`
     }
 body{
     font-family: sans-serif;
+    background: #eee;
 }
 html, body, #root{
     height: 100%;
@@ -22,4 +23,13 @@ a{
 ul{
     list-style: none;
 }
+`;
+
+export const Container = styled.section`
+    max-width: 360px;
+    background: #fff;
+    margin: 30px auto;
+    padding: 30px;
+    border-radius: 4px;
+    box-shadow: 0 0 10px rgba(0,0,0, 0.1);
 `;
