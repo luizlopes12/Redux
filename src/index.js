09e2b9f1088e5892d90774b1ca52ebaa045d8218
Luiz Lopes <luizlopes12@outlook.com>
2022-03-07 12:37:08 -0300

    lendo dados da store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 
 render(
+	<Provider store={store}>
 	<BrowserRouter>
 		<App />
-	</BrowserRouter>,
+	</BrowserRouter>
+	</Provider>,
 	document.getElementById('root')
 );
