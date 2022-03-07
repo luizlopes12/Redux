600c654a2a0bf02ac62b59a0d705b4fb3784e32c
Luiz Lopes <luizlopes12@outlook.com>
2022-03-07 14:19:18 -0300

    adicionando dados na store

--- src/pages/Add/index.js
+++ src/pages/Add/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { addCar } from '../../store/cars';
+import { useDispatch } from 'react-redux';
 export default function Add() {
+
+	const [form, setForm] = useState({name: '', url: ''})
+	const dispatch = useDispatch()
+	const formChange = (e) =>{
+		setForm({...form, [e.target.name]: e.target.value})
+	}
+	const onSubmit = (e) =>{
+		e.preventDefault()
+		dispatch(addCar(form))
+		setForm({name: '', url:''})
+	}
 	return (
-		<form className="container mt-5">
+		<form className="container mt-5" onSubmit={onSubmit}>
 			<div className="form-group">
 				<label>Nome</label>
-				<input type="text" name="name" className="form-control" placeholder="Nome..." />
+				<input type="text" onChange={formChange} name="name" value={form.name} className="form-control" placeholder="Nome..." />
 			</div>
 			<div className="form-group">
 				<label>URL:</label>
-				<input type="text" name="url" className="form-control" placeholder="URL:https://cars" />
+				<input type="text" onChange={formChange} name="url" value={form.url} className="form-control" placeholder="URL:https://cars" />
 			</div>
 			<button type="submit" className="btn btn-primary">
 				Adicionar
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -3,7 +3,7 @@ import Car from '../../components/Car';
 import { useSelector } from 'react-redux';
 
 export default function List() {
-	const cars = useSelector(state => state)
+	const cars = useSelector(state => state.cars)
 	
 	return (
 		<div className="container-fluid">
