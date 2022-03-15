07ba65385b3e025f6dd0dfe542d4a4e42c91229b
Luiz Lopes <luizlopes12@outlook.com>
2022-03-15 11:55:51 -0300

    Até que enfim, jesus

--- cart/src/pages/Cart/index.jsx
+++ cart/src/pages/Cart/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
-const Cart = () => {
+import { connect } from 'react-redux'
+const Cart = ({cartItems, dispatch}) => {
   return (
-    <div style={{height: '94vh', width: '100%', background: 'blue'}}></div>
+    <div style={{height: '94vh', width: '100%', background: 'blue'}}>
+      <ul>
+        {cartItems!== undefined && cartItems.map((item)=>(
+          <li key={item.id}>{item.name} <br/> {item.desc}</li>
+        ))}
+      </ul>
+
+    </div>
   )
 }
+const mapStateToProps = (state) =>{
+  return {cartItems: state.cartReducer.items}
+}
 
-export default Cart
+export default connect(mapStateToProps)(Cart)
--- cart/src/store/reducers/cartReducer.js
+++ cart/src/store/reducers/cartReducer.js
@@ -1,9 +1,14 @@
-const INITIAL_STATE = {}
+const INITIAL_STATE = {
+    items: [
+
+    ]
+
+    
+}
 const cartReducer = (state = INITIAL_STATE, action) =>{
     if(action.type === 'ADD_CART'){
-        console.log('opa')
+        return {items: [{id: action.item.id, name: action.item.name, desc: action.item.desc}, ...state.items]}
     }
-
     return state
 }
 
